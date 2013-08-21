bfe174783876bfc03d5a91358562fd615d0094a6
Anton Belousov <erbol.turburgaev@gmail.com>
2013-08-21 04:56:21 +0400

    Changed backup script.

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 DATE=`date '+%Y-%m-%d_%H%M'`
+STATIC_SOURCE="/opt/gtta/"
+BACKUPS_LOCATION="/var/backups/gtta"
+DATABASE_BACKUP="$BACKUPS_LOCATION/db"
+STATIC_BACKUP="$BACKUPS_LOCATION/static"
 
-# DB
-PGPASSWORD="123" pg_dump -U gtta gtta | gzip -9 > /var/backups/gtta/db/$DATE.sql.gz
-
-# FILES
-STATIC_SRC="/opt/gtta/web/"
-STATIC_DST="/var/backups/gtta/static"
+# database
+DB_PASSWORD=$(sed -n '{N;s/.*\[database\]\npassword *= *\( *\)/\1/p}' < /opt/gtta/config/gtta.ini)
+PGPASSWORD="$DB_PASSWORD" pg_dump -U gtta gtta | gzip -9 > $DATABASE_BACKUP/$DATE.sql.gz
 
 # check if file exists
-if [ ! -e $STATIC_DST/current ]
+if [ ! -e $STATIC_BACKUP/current ]
 then
-    ln -s $STATIC_SRC $STATIC_DST/current
+    ln -s $STATIC_SOURCE $STATIC_BACKUP/current
 fi
 
-rsync -az --partial --link-dest=$STATIC_DST/current $STATIC_SRC $STATIC_DST/$DATE
-rm -f $STATIC_DST/current
-ln -s $STATIC_DST/$DATE $STATIC_DST/current
-
+rsync -az --partial --link-dest=$STATIC_BACKUP/current $STATIC_SOURCE $STATIC_BACKUP/$DATE
+rm -f $STATIC_BACKUP/current
+ln -s $STATIC_BACKUP/$DATE $STATIC_BACKUP/current
