b888ed3f62a7ea165bbe04a627487472fabf2977
Anton Belousov <anton@stellarbit.com>
2014-03-23 02:51:57 +0400

    GTTA-209

---
+++ deploy/scripts/1.11/install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+# apache config
+cat > /etc/apache2/sites-available/gtta-ssl <<\EOT
+<VirtualHost *:443>
+	ServerAdmin webmaster@localhost
+
+	SSLEngine On
+	SSLCertificateFile /opt/gtta/security/ssl/gtta.crt
+	SSLCertificateKeyFile /opt/gtta/security/ssl/gtta.key
+	SSLVerifyClient none
+
+	DocumentRoot /opt/gtta/current/web
+
+	<Directory />
+		Options FollowSymLinks
+		AllowOverride All
+	</Directory>
+
+	<Location /verify>
+		SSLCACertificateFile /opt/gtta/security/ca/ca.crt
+		SSLVerifyClient optional
+		SSLVerifyDepth 10
+		SSLOptions +StdEnvVars
+		SSLRenegBufferSize 100486000
+	</Location>
+
+	<Directory /opt/gtta/current/web/>
+		Options Indexes FollowSymLinks MultiViews
+		AllowOverride All
+		Order allow,deny
+		allow from all
+	</Directory>
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+
+	# Possible values include: debug, info, notice, warn, error, crit,
+	# alert, emerg.
+	LogLevel warn
+
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOT
+
+/etc/init.d/apache2 restart
---
+++ deploy/scripts/1.11/revert.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+# apache config
+cat > /etc/apache2/sites-available/gtta-ssl <<\EOT
+<VirtualHost *:443>
+	ServerAdmin webmaster@localhost
+
+	SSLEngine On
+	SSLCertificateFile /opt/gtta/security/ssl/gtta.crt
+	SSLCertificateKeyFile /opt/gtta/security/ssl/gtta.key
+	SSLCACertificateFile /opt/gtta/security/ca/ca.crt
+
+	DocumentRoot /opt/gtta/current/web
+
+	<Directory />
+		Options FollowSymLinks
+		AllowOverride All
+		SSLVerifyClient optional
+		SSLVerifyDepth 10
+		SSLOptions +StdEnvVars
+		SSLRenegBufferSize 100486000
+	</Directory>
+
+	<Directory /opt/gtta/current/web/>
+		Options Indexes FollowSymLinks MultiViews
+		AllowOverride All
+		Order allow,deny
+		allow from all
+	</Directory>
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+
+	# Possible values include: debug, info, notice, warn, error, crit,
+	# alert, emerg.
+	LogLevel warn
+
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOT
+
+/etc/init.d/apache2 restart
