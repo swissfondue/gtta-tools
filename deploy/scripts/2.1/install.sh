a568a234337642e14e1ad9a179ce5774f6e50d0a
Anton Belousov <anton@belousov.co>
2016-07-19 21:48:43 +0300

    GTTA-502

--- deploy/scripts/2.1/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pushd /opt/gtta/current/web/protected
-./yiic hostresolve
-popd
-
-echo '0 * * * *     root    cd /opt/gtta/current/web/protected && ./yiic hostresolve' >> /etc/crontab
-/etc/init.d/cron restart
--- deploy/scripts/2.1/revert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i '/yiic hostresolve/d' /etc/crontab
