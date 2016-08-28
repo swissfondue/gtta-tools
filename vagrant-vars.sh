5894669fc80ca0ba51595293cfc760a7ae1fe83e
Anton Belousov <anton@belousov.co>
2016-08-28 03:11:39 +0300

    Vagrant vars.

--- vagrant-vars.sh
+++ vagrant-vars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export GTTA_WEB_PATH="/Users/anton/Public/Projects/gtta/web"
-export GTTA_SCRIPTS_PATH="/Users/anton/Public/Projects/gtta/scripts"
-export GTTA_TOOLS_PATH="/Users/anton/Public/Projects/gtta/tools"
-export GTTA_COMMUNITY_PATH="/Users/anton/Public/Projects/community"
-export GTTA_LUST_PATH="/Users/anton/Public/Projects/lust"
+export GTTA_WEB_PATH="/Users/anton/Projects/gtta/src/web"
+export GTTA_SCRIPTS_PATH="/Users/anton/Projects/gtta/src/scripts"
+export GTTA_TOOLS_PATH="/Users/anton/Projects/gtta/src/tools"
+export GTTA_COMMUNITY_PATH="/Users/anton/Projects/gtta/src/community"
+export GTTA_LUST_PATH="/Users/anton/Projects/lust/src"
 export GTTA_IP="192.168.77.77"
